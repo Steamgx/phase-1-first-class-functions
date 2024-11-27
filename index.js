d71ca1fd28a46e40ddb895856cd4c6fa81c19b47
Steamgx <your-bmuraya646@gmail.com>
2024-11-27 15:15:29 +0300

    complete

--- index.js
+++ index.js
@@ -1,68 +1,28 @@
-// Function that accepts a callback and calls it
 function receivesAFunction(callback) {
-    callback();
+  callback()
+}
+function returnsANamedFunction() {
+  return function named() {
+    console.log("The girl is Arya.")
   }
-  
-  // Function that returns a named function
-  function returnsANamedFunction() {
-    return function namedFunction() {
-      console.log("I am a named function!");
-    };
+}
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log('The girl has no name.')
   }
-  
-  // Function that returns an anonymous function
-  function returnsAnAnonymousFunction() {
-    return function() {
-      console.log("I am an anonymous function!");
-    };
-  }
-  
-  // Tests using Mocha and Chai
-  const chai = require("chai");
-  const spies = require("chai-spies");
-  chai.use(spies);
-  
-  describe("index", () => {
-    describe("receivesAFunction(callback)", () => {
-      it("receives a function and calls it", () => {
-        const spy = chai.spy();
-  
-        receivesAFunction(spy);
-  
-        expect(spy).to.have.been.called();
-      });
-    });
-  
-    describe("returnsANamedFunction()", () => {
-      var fn;
-  
-      before(() => {
-        fn = returnsANamedFunction();
-      });
-  
-      it("returns a function", () => {
-        expect(fn).to.be.a("function");
-      });
-  
-      it("returns a named function", () => {
-        expect(fn.name).not.to.eql(""); // Ensure the function has a name
-      });
-    });
-  
-    describe("returnsAnAnonymousFunction()", () => {
-      var fn;
-  
-      before(() => {
-        fn = returnsAnAnonymousFunction();
-      });
-  
-      it("returns a function", () => {
-        expect(fn).to.be.a("function");
-      });
-  
-      it("returns an anonymous function", () => {
-        expect(fn.name).to.eql(""); // Ensure the function doesn't have a name
-      });
-    });
-  });
-  
+}
+console.log('Starting execution of receivesAFunction');
+receivesAFunction(function() {
+  console.log('Callback executed');
+});
+console.log('Execution of receivesAFunction completed');
+
+console.log('Starting execution of returnsANamedFunction');
+var namedFunction = returnsANamedFunction();
+namedFunction();
+console.log('Execution of returnsANamedFunction completed');
+
+console.log('Starting execution of returnsAnAnonymousFunction');
+var anonymousFunction = returnsAnAnonymousFunction();
+anonymousFunction();
+console.log('Execution of returnsAnAnonymousFunction completed');
